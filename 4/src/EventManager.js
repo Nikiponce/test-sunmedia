af77d12997fd66c144731abe5f5a82c097a32691
Niki Ponce <njponce@dentix.es>
2019-05-03 10:58:52 +0200

    ;)

--- 4/src/EventManager.js
+++ 4/src/EventManager.js
@@ -1,6 +1,32 @@
 export default class EventManager{
+    constructor(events, types){
+        this.events = events;
+        this.types = types;
+    }
+    
     run() {
-        // implement your code here...
         console.log("RUNNING");
+        this.timer();
+    }
+
+    timer(){
+        this.second = 0;
+        this.interval = setInterval(()=>{
+
+            this.checkSecond();
+
+            this.second++;
+        }, 1000);
+        
+    }
+
+    checkSecond(){
+        if (this.second >= 10)  clearInterval(this.interval);
+        this.events.filter((item)=>{
+            return (this.second == item.second);
+        }).map((item)=>{
+            delete item.second;
+            return (this.types.includes(item.type)) ? console.log(`At second ${this.second}: `, item) : '';
+        });
     }
 };
--- 4/src/EventManagerFactory.js
+++ 4/src/EventManagerFactory.js
@@ -3,7 +3,9 @@ import Event from './Event';
 
 export default class EventManagerFactory{
     static create(events, types) {
-        // implement your code here...
-        return new EventManager();
+        events = events.map((item) => {
+            return new Event(item.second, item.type, item.message);
+        });
+        return new EventManager(events, types);
     }
 };
